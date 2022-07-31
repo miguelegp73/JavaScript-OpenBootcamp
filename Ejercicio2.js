59c09edc447ca812d91b788a460681d7fcb98594
Your Name <you@example.com>
2022-07-31 19:35:50 -0300

    commit ejerccio2

---
+++ Ejercicio2.js
@@ -0,0 +1,9 @@
+const lista = ["Miguel", 49, true, 17 / 08 / 1973, JS_desde_Cero = {
+    titulo: "JS desde cero",
+    autor: "Miguel Garcia",
+    fecha: 17 / 08 / 1973,
+    url: "https:\\wwww.tulibro.com"
+    
+}]
+
+console.log(lista);
