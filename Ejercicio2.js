2d462d9f46a4a0e71700f28811f47b71ad005c1a
Your Name <you@example.com>
2022-07-31 19:51:53 -0300

    Ejercicio2 3er commit

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -2,12 +2,12 @@ const lista = [
     "Miguel",
     49,
     true,
-    new Date(1972, 08, 17),
+    new Date(1973, 08, 17),
 
     tuLibroFavorito = {
     titulo: "JS desde cero",
     autor: "Miguel Garcia",
-    fecha: 17 / 08 / 1973,
+    fecha: new Date (17, 02, 1962),
     url: "https:\\wwww.tulibro.com"
     
     }]
