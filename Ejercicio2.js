88d003b5be7a0a27c17d35e314aee744a276b56d
Your Name <you@example.com>
2022-07-31 19:46:19 -0300

    Ejercicio2 2do commit

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,9 +1,13 @@
-const lista = ["Miguel", 49, true, 17 / 08 / 1973, JS_desde_Cero = {
+const lista = [
+    "Miguel",
+    49,
+    true,
+    new Date(1972, 08, 17),
+
+    tuLibroFavorito = {
     titulo: "JS desde cero",
     autor: "Miguel Garcia",
     fecha: 17 / 08 / 1973,
     url: "https:\\wwww.tulibro.com"
     
-}]
-
-console.log(lista);
+    }]
